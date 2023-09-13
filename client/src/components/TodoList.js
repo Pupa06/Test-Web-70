4479abf365476592f72db2082eec25c34836e785
Pupa06 <126087025+Pupa06@users.noreply.github.com>
2023-09-14 01:17:10 +0700

    update

--- client/src/components/TodoList.js
+++ client/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import { FaRegCircle, FaRegCheckCircle } from "react-icons/fa";
 import { useEffect, useState } from "react";
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const TodoList = ({ notDone }) => {
   const [todos, setTodos] = useState([]);
@@ -19,7 +20,18 @@ const TodoList = ({ notDone }) => {
     localStorage.setItem("tasks", JSON.stringify(updatedTodos));
   };
 
-  const filteredTodos = notDone ? todos.filter((todo) => !todo.completed) : todos
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+
+    const updatedTodos = [...todos];
+    const [reorderedItem] = updatedTodos.splice(result.source.index, 1);
+    updatedTodos.splice(result.destination.index, 0, reorderedItem);
+
+    setTodos(updatedTodos);
+    localStorage.setItem("tasks", JSON.stringify(updatedTodos));
+  };
+
+  const filteredTodos = notDone ? todos.filter((todo) => !todo.completed) : todos;
 
   const getRemainingDays = (dueDate) => {
     const today = new Date();
@@ -28,34 +40,42 @@ const TodoList = ({ notDone }) => {
     const remainingDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
     return remainingDays;
   };
+
   return (
-    <div className="todo-list-container">
-      {filteredTodos.map((todo) => {
-        const taskClassname = todo.completed
-          ? "todo-item-container done"
-          : "todo-item-container";
-        return (
-          <div
-            style={{ display: "flex", justifyContent: "space-between" }}
-            className={taskClassname}
-            key={todo.id}
-            onClick={() => toggleTodo(todo.id)}
-          >
-            <div style={{ display: "flex" }}>
-              {todo.completed ? (
-                <FaRegCheckCircle color="#9a9a9a" className="item-done-button" />
-              ) : (
-                <FaRegCircle className="item-done-button" color="#9a9a9a" />
-              )}
-              <div className="item-title">{todo.title}</div>
-            </div>
-            <div className="remaining-days">
-              {getRemainingDays(todo.dueDate)} days left
-            </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="todos">
+        {(provided) => (
+          <div {...provided.droppableProps} ref={provided.innerRef} className="todo-list-container">
+            {filteredTodos.map((todo, index) => (
+              <Draggable key={todo.id} draggableId={todo.id} index={index}>
+                {(provided) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className={todo.completed ? "todo-item-container done" : "todo-item-container"}
+                    onClick={() => toggleTodo(todo.id)}
+                  >
+                    <div style={{ display: "flex" }}>
+                      {todo.completed ? (
+                        <FaRegCheckCircle color="#9a9a9a" className="item-done-button" />
+                      ) : (
+                        <FaRegCircle className="item-done-button" color="#9a9a9a" />
+                      )}
+                      <div className="item-title">{todo.title}</div>
+                    </div>
+                    <div className="remaining-days">
+                      {getRemainingDays(todo.dueDate)} ngày còn lại
+                    </div>
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
           </div>
-        );
-      })}
-    </div>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
 
